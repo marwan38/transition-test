03a5efd617fd86c4ab91fb571b8c677be82fc178
Saadoun Baydee <saadoun@Saadouns-MacBook-Pro.local>
2020-03-15 20:23:00 +1300

    build

---
+++ build/precache-manifest.40be35d2c4128ce5efe04cfd1f1e9278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b796522c70bc14d011fc81c5fc069f0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e990d9f3d0a28c4fcd71",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9c0097d6bc45c1062d04",
+    "url": "/static/js/2.f4771556.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.f4771556.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e990d9f3d0a28c4fcd71",
+    "url": "/static/js/main.b9666c44.chunk.js"
+  },
+  {
+    "revision": "c62b98984113b555b8d7",
+    "url": "/static/js/runtime-main.330ef975.js"
+  }
+]);
